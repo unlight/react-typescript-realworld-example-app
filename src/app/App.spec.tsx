eec87a9b908edeae7a003897208bd302fb39028f
Roman_Vasilev <s91630277@gmail.com>
2021-08-08 00:29:26 +0400

    test: Fix unit tests

--- src/app/App.spec.tsx
+++ src/app/App.spec.tsx
@@ -1,17 +1,30 @@
 import { Interface } from '@libs/application';
 import { render, waitFor } from '@testing-library/react';
 import { mock } from 'jest-mock-extended';
-import { injector } from 'njct';
+import { inject, injector } from 'njct';
 import React from 'react';
 
 import { App } from './App';
 
 jest.mock('./home/PopularTags', () => ({ PopularTags: () => 'populartags' }));
 
-it('renders app component', async () => {
-    const articleService = mock<Interface.ArticleService>();
-    articleService.findMany.mockResolvedValue({ articles: [], articlesCount: 0 });
+let articleService = mock<Interface.ArticleService>();
+let sessionService = mock<Interface.SessionService>();
+
+beforeEach(() => {
+    articleService = mock<Interface.ArticleService>();
+    sessionService = mock<Interface.SessionService>();
     injector.provide('articleservice', () => articleService);
+    injector.provide('sessionservice', () => sessionService);
+
+    articleService.findMany.mockResolvedValue({ articles: [], articlesCount: 0 });
+});
+
+afterAll(() => {
+    injector.clear();
+});
+
+it('renders app component', async () => {
     const screen = render(<App />);
     await waitFor(() => {
         expect(screen.getByText('Home')).toBeInTheDocument();
@@ -19,9 +32,6 @@ it('renders app component', async () => {
 });
 
 it('renders all app html', async () => {
-    const articleService = mock<Interface.ArticleService>();
-    articleService.findMany.mockResolvedValue({ articles: [], articlesCount: 0 });
-    injector.provide('articleservice', () => articleService);
     const screen = render(<App />);
     await waitFor(() => {
         expect(screen.baseElement.outerHTML).toBeTruthy();
