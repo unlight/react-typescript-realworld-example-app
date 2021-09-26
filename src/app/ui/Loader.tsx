7872f44cc26a50019a780520fd45abb661c3d86f
Roman_Vasilev <s91630277@gmail.com>
2021-09-26 17:19:30 +0400

    fix: React warning

--- src/app/ui/Loader.tsx
+++ src/app/ui/Loader.tsx
@@ -1,39 +1,34 @@
-import React, { PropsWithChildren } from 'react';
+import React from 'react';
 import { atom, useRecoilValue } from 'recoil';
-
-type LoaderProps = {};
 
 export const isLoading = atom({
   key: 'appIsLoading',
   default: false,
 });
 
-export function Loader(props: PropsWithChildren<LoaderProps>) {
+export function Loader() {
   const showLoading = useRecoilValue(isLoading);
 
-  return showLoading ? <Loading /> : null;
+  return showLoading ? <>{Loading}</> : null;
 }
 
-export function Loading() {
-  return (
-    <div className="flex justify-center w-full h-full absolute inset-0 bg-opacity-25 bg-gray-500">
-      <div className="flex space-x-1 text-sm text-gray-700 pt-10">
-        <svg
-          fill="none"
-          className="w-6 h-6 animate-spin"
-          viewBox="0 0 32 32"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            clipRule="evenodd"
-            d="M15.165 8.53a.5.5 0 01-.404.58A7 7 0 1023 16a.5.5 0 011 0 8 8 0 11-9.416-7.874.5.5 0 01.58.404z"
-            fill="currentColor"
-            fillRule="evenodd"
-          />
-        </svg>
-
-        <div>Loading ...</div>
-      </div>
+export const Loading = (
+  <div className="flex justify-center w-full h-full absolute inset-0 bg-opacity-25 bg-gray-500">
+    <div className="flex space-x-1 text-sm text-gray-700 pt-10">
+      <svg
+        fill="none"
+        className="w-6 h-6 animate-spin"
+        viewBox="0 0 32 32"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          clipRule="evenodd"
+          d="M15.165 8.53a.5.5 0 01-.404.58A7 7 0 1023 16a.5.5 0 011 0 8 8 0 11-9.416-7.874.5.5 0 01.58.404z"
+          fill="currentColor"
+          fillRule="evenodd"
+        />
+      </svg>
+      <div>Loading ...</div>
     </div>
-  );
-}
+  </div>
+);
