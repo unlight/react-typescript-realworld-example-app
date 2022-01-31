9a3c407312aad34ac27ea64c14447482283adc0a
Roman_Vasilev <s91630277@gmail.com>
2022-01-31 12:11:04 +0400

    chore: Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mode: 'aot',
-  purge: ['./index.html', './src/**/*.tsx'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./index.html', './src/**/*.tsx'],
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
   },
